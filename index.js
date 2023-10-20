c4c0f9a72fe7bc1c5640ff3bb15112e3d30dc84b
unknown <bijoordzen@gmail.com>
2023-10-20 13:45:35 +0530

    added delete button feature

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 var button = document.getElementById("enter")
 var input = document.getElementById("userinput")
 // querying elements by using id in html 
+let button2 = document.createElement('BUTTON');
+let text = document.createTextNode("DELETE");
+button2.appendChild(text);
+
+
+
 var ul = document.querySelector("ul")
 button.addEventListener("click", function(){
     if (input.value.length > 0){
@@ -9,7 +15,14 @@ button.addEventListener("click", function(){
         li.appendChild(document.createTextNode(input.value))
         //see parent child hierarchiy picture of dom basics
         ul.appendChild(li)
+        li.appendChild(button2)
         //empty string after submission
         input.value = ""
     }
+li.addEventListener("click",function(){
+    li.classList.toggle("done");
+})
+button2.addEventListener("click",function(){
+    button2.remove();
+})
 })
